463dc09401ee4ad5bc93dfa2e3a27c3e2b06e01a
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-04-08 14:23:21 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 // options
 const CLIENT_ID = '167153388710-abq6ngmdvbqjselggev0e3ui1ovidm69.apps.googleusercontent.com';
-const DISCOVERY_DOCS = ["https://www.googleapis.com/"]
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+
+const authorizeButton = document.getElementById('authorize-button');
+const signoutButton = document.getElementById('signout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
+
+// Load auth2 library
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+// init 
